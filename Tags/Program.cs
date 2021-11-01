8ec5a818b95fb0d896d0cfe5039715264d808557
Dan nguyen <danek.nguyen@gmail.com>
2021-11-01 23:18:20 +0100

    Přidejte soubory projektu.

---
+++ Tags/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Tags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static string UpperCase(string s)
+        { 
+            return s.ToUpper(); 
+        }
+
+        static string LowerCase(string s)
+        { 
+            return s.ToLower(); 
+        }
+
+        static string Break(string s)
+        {
+            return s;
+        }
+
+        static string NoSpaces(string s)
+        {
+            return s;
+        }
+    }
+}
